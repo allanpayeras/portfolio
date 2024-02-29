d498f33d520ba9a668dd7ea30833a963adb3e6ae
Allan Payeras <allan.payeras@gmail.com>
2024-02-29 14:59:27 -0800

    revamp code using langchain

--- summarize_pdf/summarize.py
+++ summarize_pdf/summarize.py
@@ -1,170 +1,97 @@
-import argparse
-import subprocess
+from dotenv import load_dotenv
+import argparse as ap
 import os
-import sys
-import requests
-import logging
+
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+from langchain.chains import LLMChain
+from langchain_core.runnables import RunnablePassthrough
 
 from gtts import gTTS
 
-logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
+TEXT_CHUNK_SIZE = 10000
 
 
-def pdf_to_mmd(input_file):
-    logging.info(f"Extracting text from {input_file}.")
-    command = ["nougat", input_file, "-o", "./"]
-    try:
-        subprocess.run(command, check=True)
-    except:
-        logging.exception(f"Error while extracting text from {input_file}.")
-        sys.exit(1)
+def concatenate_documents(documents: list) -> str:
+    text = ""
+    for document in documents:
+        text += document.page_content
+    return text
 
 
-class MmdDocument:
-    def __init__(self, mmd_file):
-        self.file = mmd_file
-        self.n_sec = 0
-        self.document = {}
-        self.has_title = False
-        self.has_abstract = False
-        self.has_sections = False
-        self.has_misc = False
-        self.parse_document()
-
-    def parse_document(self):
-        with open(self.file) as f:
-            line = f.readline()
-            while line:
-                if self.is_title_start(line):
-                    self.has_title = True
-                    self.document["title"] = line
-                    line = f.readline()
-                    while not (
-                        self.is_abstract_start(line)
-                        or self.is_section_start(line)
-                        or self.is_eod(line)
-                    ):
-                        self.document["title"] += line
-                        line = f.readline()
-                elif self.is_abstract_start(line):
-                    self.has_abstract = True
-                    self.document["abstract"] = line
-                    line = f.readline()
-                    while not (
-                        self.is_title_start(line)
-                        or self.is_section_start(line)
-                        or self.is_eod(line)
-                    ):
-                        self.document["abstract"] += line
-                        line = f.readline()
-                elif self.is_section_start(line):
-                    self.has_sections = True
-                    self.document[f"section_{self.n_sec}"] = line
-                    line = f.readline()
-                    while not (
-                        self.is_title_start(line)
-                        or self.is_abstract_start(line)
-                        or self.is_section_start(line)
-                        or self.is_eod(line)
-                    ):
-                        self.document[f"section_{self.n_sec}"] += line
-                        line = f.readline()
-                    self.n_sec += 1
-                else:
-                    self.has_misc = True
-                    self.document["misc"] = line
-                    line = f.readline()
-                    while not (
-                        self.is_title_start(line)
-                        or self.is_abstract_start(line)
-                        or self.is_section_start(line)
-                        or self.is_eod(line)
-                    ):
-                        self.document[f"misc"] += line
-                        line = f.readline()
-
-    @staticmethod
-    def is_title_start(line):
-        return line.startswith("# ")
-
-    @staticmethod
-    def is_abstract_start(line):
-        return "###### Abstract" in line
-
-    @staticmethod
-    def is_section_start(line):
-        return line.startswith("## ")
-
-    @staticmethod
-    def is_eod(line):
-        return line == ""
+def extract_pdf_text(pdf_file: str) -> str:
+    loader = PyPDFLoader(pdf_file)
+    documents = loader.load()
+    return concatenate_documents(documents)
 
 
-def summarize_text(mmd_doc, api_key):
-    logging.info("Summarizing text using NVIDIA-LLAMA-2 model.")
-    invoke_url = "https://api.nvcf.nvidia.com/v2/nvcf/pexec/functions/7b3e3361-4266-41c8-b312-f5e33c81fc92"
-    fetch_url_format = "https://api.nvcf.nvidia.com/v2/nvcf/pexec/status/"
-    headers = {
-        "Authorization": f"Bearer {api_key}",
-        "Accept": "application/json",
-    }
+def split_text(text: str) -> list[str]:
+    splitter = CharacterTextSplitter(
+        separator=".\n",
+        chunk_size=TEXT_CHUNK_SIZE,
+        chunk_overlap=0,
+        length_function=len,
+    )
+    text_chunks = splitter.split_text(text)
 
-    session = requests.Session()
+    # Sometimes CharacterTextSplitter returns text chunks larger than specified by chunk_size.
+    # Here we exclude those so that there is no risk of inputs exceeding the
+    # context window of our LLM.
+    return [chunk for chunk in text_chunks if len(chunk) < TEXT_CHUNK_SIZE]
 
-    payload = {
-        "messages": [
-            {
-                "content": f"Summarize the following text: {mmd_doc.document['section_0']}",
-                "role": "user",
-            }
-        ],
-        "temperature": 0.5,
-        "top_p": 0.7,
-        "max_tokens": 1024,
-        "stream": False,
-    }
 
+def get_summarization_chain():
+    prompt_template = """You are an academic professor with many years of experience in summarizing academic texts with detailed description of the main points. Summarize the following text keeping a clear and in-depth description of the main ideas. Ignore parts of the text that contain useless information such as acknowledgements, list of publications, references, list of author names and institutions. If the text provided only contains useless information such as acknowledgements, or list of publications, or list of author names, or institutions return 'NOT USEFUL'. Provide the output without linebreaks.
+    
+    TEXT: {text}
+    
+    OUTPUT:"""
+    prompt = ChatPromptTemplate.from_template(prompt_template)
+
+    llm_summarizer = ChatOpenAI(model="gpt-3.5-turbo-0125", temperature=0.2)
+
+    return {"text": RunnablePassthrough()} | LLMChain(llm=llm_summarizer, prompt=prompt)
+
+
+def summarize_chunks(text_chunks: list[str]) -> str:
+    summarization_chain = get_summarization_chain()
+    summaries = summarization_chain.batch(text_chunks)
     summary = ""
-    for i in range(1, mmd_doc.n_sec):
-        response = session.post(invoke_url, headers=headers, json=payload)
-
-        while response.status_code == 202:
-            request_id = response.headers.get("NVCF-REQID")
-            fetch_url = fetch_url_format + request_id
-            response = session.get(fetch_url, headers=headers)
-
-        response.raise_for_status()
-        response_body = response.json()["choices"][0]["message"]["content"]
-        summary += " " + response_body
-        payload["messages"][0][
-            "content"
-        ] = f"Summarize the following text: {mmd_doc.document[f'section_{i}']}"
-    logging.info("Summary complete.")
-
+    for piece in summaries:
+        if piece["text"] != "NOT USEFUL":
+            summary += " " + piece["text"]
     return summary
 
 
-def create_audio(text, output_name):
-    logging.info(f"Creating MP3 audio file '{output_name}'.")
-    myobj = gTTS(text=text, lang="en", tld="com", slow=False)
-    myobj.save(output_name)
-    logging.info("Audio file created.")
+def create_audio_file(text: str, file_name: str):
+    conclusion_text = "This summary was produced using generative artificial intelligence. Thank you for listening."
+    final_summary = text + conclusion_text
+
+    g_speech = gTTS(text=final_summary, lang="en", tld="com")
+    g_speech.save(savefile=file_name)
 
 
 def main():
-    parser = argparse.ArgumentParser()
+    load_dotenv()
+
+    parser = ap.ArgumentParser(description="Produce an audio summary of a PDF file.")
     parser.add_argument("pdf_file", help="Input PDF file to be summarized.")
-    parser.add_argument("key", help="Personal key to use NVIDIA-LLAMA-2 API.")
+    parser.add_argument(
+        "-o",
+        "--output",
+        default="summary",
+        help="Name of the output audio file without any audio extension. Defaults to 'summary'.",
+    )
     args = parser.parse_args()
 
-    pdf_to_mmd(args.pdf_file)
-    logging.info("Parsing extracted text from PDF file.")
-    mmd_file = os.path.basename(args.pdf_file).replace(".pdf", ".mmd")
-    mmd_doc = MmdDocument(mmd_file)
-    summary = summarize_text(mmd_doc, api_key=args.key)
-
-    audio_file = os.path.basename(args.pdf_file).replace(".pdf", ".mp3")
-    create_audio(summary, audio_file)
+    if os.path.exists(args.pdf_file):
+        text = extract_pdf_text(args.pdf_file)
+        text_chunks = split_text(text)
+        summary = summarize_chunks(text_chunks)
+        audio_name = args.output + ".mp3"
+        create_audio_file(summary, audio_name)
 
 
 if __name__ == "__main__":
